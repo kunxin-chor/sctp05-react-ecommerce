bcc3ce6046e95a42d5193537a3f49a93f2b8df5b
kunxin-chor <chorkunxin@yahoo.com>
2024-10-26 08:05:43 +0000

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from "./Header"
 
 import "./styles.css"
 import ProductCard from './ProductCard'
 
 export default function App() {
+
+  const handleNavButtonClick = () => {
+    // if (isNavbarOpened){
+    //   setNavBarOpen(false);
+    // }  else {
+    //   setNavBarOpen(true);
+    // }
+
+    // setNavBarOpen( isNavbarOpened ? false : true);
+    setNavBarOpen(!isNavbarOpened);
+  }
+
+  const [isNavbarOpened, setNavBarOpen] = useState(false);
+
   return (
     <>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -16,10 +30,11 @@ export default function App() {
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
+          onClick={handleNavButtonClick}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className={`collapse navbar-collapse ${isNavbarOpened ? "show" : ""  }`} id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">Home</a>
