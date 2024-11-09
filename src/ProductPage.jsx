8ae0299eaa88e5ad8d9f1bb4e1e0d26f95b44348
kunxin-chor <chorkunxin@yahoo.com>
2024-11-09 06:52:03 +0000

    Added list rendering

--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+import ProductCard from './ProductCard';
 
 export default function ProductPage() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(()=>{
+
+        // The effect function cannot be async
+        const loadData = async() => {
+            // all URLs to static assets will be relative to public
+            const response  = await axios.get("products.json");
+            setProducts(response.data);
+        }
+        loadData();
+
+
+    }, [])
+
+    const renderProductsV2 = () => {
+        const jsxElements = products.map(function(p){
+            return <li key={p.id}>{p.name} ${p.price}</li>
+        })
+        return jsxElements;
+    }
+
+    const renderProducts = () => {
+        const jsxElements = [];
+        for (let p of products) {
+            jsxElements.push(<li key={p.id}>
+                {p.name} ${p.price}
+            </li>);
+        }
+        return jsxElements;
+    }
+
     return (
         <>
-        <h1>Products</h1>
+            <h1>Products</h1>
+            
+                {/* {renderProducts()} */}
+                {/* {
+                    (()=>{
+                        const jsxElements = [];
+                        for (let p of products) {
+                            jsxElements.push(<li key={p.id}>
+                                {p.name} ${p.price}
+                            </li>);
+                        }
+                        return jsxElements;
+                    })()
+                } */}
+
+                <div className="row">
+                    { products.map(p => 
+                        <div className="col-12 col-md-4 col-lg-3">
+                        <ProductCard
+                            productName = {p.name}
+                            price = {p.price}
+                            imageUrl = {p.image}
+                            id = {p.id}
+                            description={p.description}
+                          />
+                          </div>
+                    )}
+                </div>
+
+            
         </>
     )
 }
